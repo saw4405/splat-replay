075256de096532f7fcfcbdc5a257a6992128417d
saw4405 <shogo_sawasaki@jp.honda>
2025-01-04 15:43:33 +0900

    処理高速化のため、グレースケールで一致確認できるように変更

--- template_matcher.py
+++ template_matcher.py
@@ -16,37 +16,40 @@ class TemplateMatcher:
         self._template = cv2.imread(template_path)
         if self._template is None:
             raise ValueError(f"Failed to load template image: {template_path}")
-        
-        self._mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE) if mask_path else None
+        self._template_gray = cv2.cvtColor(self._template, cv2.COLOR_BGR2GRAY)
+
+        self._mask = cv2.imread(
+            mask_path, cv2.IMREAD_GRAYSCALE) if mask_path else None
         if mask_path and self._mask is None:
             raise ValueError(f"Failed to load mask image: {mask_path}")
 
         self._threshold = threshold
-        
+
         self.height, self.width = self._template.shape[:2]
 
-    def match(self, frame: np.ndarray) -> Tuple[bool, Optional[Tuple[int, int]]]:
+    def match(self, frame: np.ndarray, use_grayscale: bool = True) -> Tuple[bool, Optional[Tuple[int, int]]]:
         """
         フレーム内でテンプレートを探し、一致したかどうかを返す。
 
         :param frame: 検索対象のフレーム。
+        :param use_grayscale: グレースケールでテンプレートマッチングを行うかどうか。
         :return: (一致フラグ, 一致した位置) のタプル。位置は (x, y) の座標。
         """
         if not isinstance(frame, np.ndarray):
             raise TypeError("Frame must be a numpy ndarray.")
 
-        # テンプレートマッチングを実行
-        if self._mask is not None:
-            result: np.ndarray = cv2.matchTemplate(
-                frame, self._template, cv2.TM_CCOEFF_NORMED, mask=self._mask
-            )
+        if use_grayscale:
+            # グレースケールに変換して処理を高速化
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            template = self._template_gray
         else:
-            result: np.ndarray = cv2.matchTemplate(
-                frame, self._template, cv2.TM_CCOEFF_NORMED
-            )
+            template = self._template
+
+        # テンプレートマッチングを実行
+        result = cv2.matchTemplate(
+            frame, template, cv2.TM_CCOEFF_NORMED, mask=self._mask)
 
         _, max_val, _, max_loc = cv2.minMaxLoc(result)
 
         # 一致が閾値以上かどうかを判定
         return max_val >= self._threshold, max_loc if max_val >= self._threshold else None
-    
